dd452e683823e1d4f542b32efae988283a9374b5
Jian Zhang <zjsariel@gmail.com>
2024-02-02 15:58:12 +0800

    remove unused files

--- third-party/glog.cmake
+++
@@ -1,3 +0,0 @@
-include(${CMAKE_SOURCE_DIR}/cmake/Modules/LeanStoreAddExternalLib.cmake)
-add_definitions(-DGLOG_CUSTOM_PREFIX_SUPPORT)
-leanstore_add_ext_lib(glog libglog.so "https://github.com/google/glog.git" v0.6.0)
