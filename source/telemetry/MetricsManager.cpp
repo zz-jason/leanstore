4a925214f103978e44d73cd8a42b3e09f72e4a05
Jian Zhang <zjsariel@gmail.com>
2024-02-02 22:13:09 +0800

    update to vcpkg

--- source/telemetry/MetricsManager.cpp
+++ source/telemetry/MetricsManager.cpp
@@ -2,7 +2,7 @@
 
 namespace leanstore {
 
-auto MetricsManager::sInstance =
+std::unique_ptr<MetricsManager> MetricsManager::sInstance =
     std::make_unique<MetricsManager>("127.0.0.1:10080");
 
 } // namespace leanstore
